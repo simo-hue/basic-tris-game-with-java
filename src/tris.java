c7470b67d03c0395c981e0f57f062c558735c731
MatioliSimone <simone.mattioli@marconirovereto.it>
2021-02-25 16:12:51 +0100

    Create tris.java

---
+++ src/tris.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.awt.*;
+import java.util.*;
+import javax.swing.*;
+import java.awt.event.*;
+/**
+ * 
+ * @author Sammy Guergachi <sguergachi at gmail.com>
+ */
+public class tris implements ActionListener{
+
+    public tris() {
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+       
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    public void primaMossa() {}
+    
+    public void controlla() {}
+    
+    
+
+    
+    
+}
