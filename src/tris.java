b0496bfb52c07d6a87c29b242b291461f40798c9
MatioliSimone <simone.mattioli@marconirovereto.it>
2021-02-25 16:21:18 +0100

    Update tris.java
    fine costruttore

--- src/tris.java
+++ src/tris.java
@@ -12,8 +12,33 @@ import java.awt.event.*;
  * @author Sammy Guergachi <sguergachi at gmail.com>
  */
 public class tris implements ActionListener{
-
+    
+    Random ranodm = new Random();
+    JFrame frame = new JFrame();
+    JPanel titolo = new JPanel();
+    JPanel bottone = new JPanel();
+    JLabel testo = new JLabel();
+    JButton[] bottoni = new JButton[9];
+    boolean p1Girata;
+    
     public tris() {
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(800,800);
+        frame.getContentPane().setBackground(Color.black);
+        frame.setLayout(new BorderLayout());
+        frame.setVisible(true);
+        
+        testo.setBackground(Color.red);
+        testo.setForeground(Color.green);
+        testo.setFont(new Font("Ink Free",Font.BOLD,75));
+        testo.setHorizontalAlignment(JLabel.CENTER);
+        testo.setText("TRIS");
+        testo.setOpaque(true);
+        
+        titolo.setLayout(new BorderLayout());
+        titolo.setBounds(0,0,800,100);
+        titolo.add(testo);
+        frame.add(titolo);
     }
 
     @Override
@@ -25,6 +50,9 @@ public class tris implements ActionListener{
     public void primaMossa() {}
     
     public void controlla() {}
+    
+    public void xvittoria(int a, int b, int c) {}
+     public void ovittoria(int a, int b, int c) {}
     
     
 
